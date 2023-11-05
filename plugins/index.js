9fb8f04f17ca094d9cc8e2ac54254241ef9cd827
Sherry Bath <sherry.bath@changehealthcare.com>
2023-11-05 09:11:49 -0800

    added api spec file

---
+++ plugins/index.js
@@ -0,0 +1,40 @@
+const fs = require("fs-extra");
+const path = require("path");
+
+function getConfigurationFile(file) {
+  //const pathToConfigFile = path.resolve(".", "cypress/config", `${env}.json`)
+  const pathToConfigFile = path.resolve( "cypress/config", `${file}.json`);
+
+  return fs.readJson(pathToConfigFile);
+}
+
+// <reference types="cypress" />
+// ***********************************************************
+// This example plugins/index.js can be used to load plugins
+//
+// You can change the location of this file or turn off loading
+// the plugins file with the 'pluginsFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/plugins-guide
+// ***********************************************************
+
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
+module.exports = (on, config) => {
+  // `on` is used to hook into various events Cypress emits
+  // `config` is the resolved Cypress config
+  on("task", {
+    log (message) {
+      // eslint-disable-next-line no-console
+      console.log(message);
+      return null;
+    }
+  });
+  const file = config.env.configFile || "staging";
+  return getConfigurationFile(file);
+};
